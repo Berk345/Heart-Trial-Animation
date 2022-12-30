1560d11d836cfce6f06163bb97362fee18396fce
berk345 <berkkarabck@gmail.com>
2022-12-30 15:42:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const BodyEL = document.querySelector("body")
+
+
+BodyEL.addEventListener("mousemove", (event)=>{
+    const xPos = event.offsetX
+    const yPos = event.offsetY
+    const spanEL = document.createElement("span")
+    spanEL.style.left = xPos + "px"
+    spanEL.style.top = yPos + "px"
+    const size = Math.random()*100
+    spanEL.style.width = size + "px"
+    spanEL.style.height = size + "px"
+    BodyEL.appendChild(spanEL);
+    setTimeout(()=>{
+        spanEL.remove()
+    },3000)
+})
